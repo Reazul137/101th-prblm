1b39a90e30ffc3786be26ecfc83636a23615e3c0
Reazul Islam <reazulislam137@gmail.com>
2018-04-01 02:02:22 +0600

    first commit

---
+++ april-01-05.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"\n\n Print the sum two numbers : \n";
+    cout<<"_________________________________\n";
+
+    int a, b, sum;
+    a=29;
+    b=30;
+    sum= a + b;
+
+    cout<<"The sum of "<<a<<" and "<<b<<" is : "<<sum<<endl;
+    return 0;
+}
